baf96a15b0b2874f3407f4176de3fa30d1d74f61
KoreaHaos <koreahaos@gmail.com>
2017-03-20 14:50:14 +0000

    Fix file structure.

--- z_scripts/add_commit_push.bash
+++ z_scripts/add_commit_push.bash
@@ -3,7 +3,6 @@ function init(){
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     local DIR_TO_ACT_ON;
     DIR_TO_ACT_ON=${DIR%*/z_scripts}
-    cd $DIR_TO_ACT_ON;
     add_commit_push;
 }
 
